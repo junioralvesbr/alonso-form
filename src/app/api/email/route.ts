08768df950ef4cffb5d89c4306aab49b4701d5a3
Junior Alves <jrnalves@gmail.com>
2023-11-20 01:18:14 -0300

    send email with attachment

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+import fs from 'fs'
+
+const resend = new Resend('re_8FWisSxs_9oF6BT7JykfgUwRMcCRsNfhG')
+
+export async function POST() {
+  const fileBuffer = fs.readFileSync('src/assets/foto.png')
+
+  console.log(fileBuffer)
+
+  try {
+    resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: 'jrnalves@gmail.com',
+      subject: "Teste de Email",
+      html: '<h1>Teste de email</h1>',
+      attachments: [
+        {
+          filename: 'foto.png',
+          content: fileBuffer
+        }
+      ]
+    })
+
+    return NextResponse.json({ message: "Foi lindo" })
+
+  } catch (error) {
+
+    return NextResponse.json({ message: error })
+
+  }
+
+}
---
+++ src/components/EmailTemplate.tsx
@@ -0,0 +1,5 @@
+export function EmailTemplate() {
+  return (
+    <h1>Teste</h1>
+  )
+}
