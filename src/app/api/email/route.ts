f182e4ebd655f12f553aa4ac270d3794c6ee7ec0
Junior Alves <jrnalves@gmail.com>
2023-11-20 01:59:44 -0300

    add input form

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -4,24 +4,25 @@ import fs from 'fs'
 
 const resend = new Resend('re_8FWisSxs_9oF6BT7JykfgUwRMcCRsNfhG')
 
-export async function POST() {
+export async function POST(req, res) {
+  console.log(req)
   const fileBuffer = fs.readFileSync('src/assets/foto.png')
 
   console.log(fileBuffer)
 
   try {
-    resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: 'jrnalves@gmail.com',
-      subject: "Teste de Email",
-      html: '<h1>Teste de email</h1>',
-      attachments: [
-        {
-          filename: 'foto.png',
-          content: fileBuffer
-        }
-      ]
-    })
+    // resend.emails.send({
+    //   from: 'Acme <onboarding@resend.dev>',
+    //   to: 'jrnalves@gmail.com',
+    //   subject: "Teste de Email",
+    //   html: '<h1>Teste de email</h1>',
+    //   attachments: [
+    //     {
+    //       filename: 'foto.png',
+    //       content: fileBuffer
+    //     }
+    //   ]
+    // })
 
     return NextResponse.json({ message: "Foi lindo" })
 
